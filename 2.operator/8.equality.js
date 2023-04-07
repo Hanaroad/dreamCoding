cd7f18bfe59f383b1902526b57199a73195f7da9
jhn2762 <jhn2762@gmail.com>
2023-04-07 09:27:54 +0900

    last commit

--- 2.operator/8.equality.js
+++ 2.operator/8.equality.js
@@ -9,7 +9,7 @@ console.log(2 == 2);
 console.log(2 != 2);
 console.log(2 != 3);
 console.log(2 == 3);
-console.log(2 == '2');
+console.log(2 == '2'); // *
 console.log(2 === '2'); // *
 console.log(true == 1);
 console.log(true === 1);
